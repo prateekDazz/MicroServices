0b80e912c3cbf4a9ca28f8112ec8c708a492af1a
prateekDazz <146050363+prateekDazz@users.noreply.github.com>
2024-02-17 19:40:45 +0530

    update Accounts Microservice

--- Accounts/MicroService/src/main/java/com/microservice/MicroServiceApplication.java
+++ Accounts/MicroService/src/main/java/com/microservice/MicroServiceApplication.java
@@ -1,13 +1,16 @@
 package com.microservice;
 
+import com.microservice.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @OpenAPIDefinition(
 		info=@Info(
 				title = "Accounts Microservice RestAPI Documentation",
--- Accounts/MicroService/src/main/java/com/microservice/controller/AccountsController.java
+++ Accounts/MicroService/src/main/java/com/microservice/controller/AccountsController.java
@@ -1,10 +1,7 @@
 package com.microservice.controller;
 
 import com.microservice.constants.AccountsConstants;
-import com.microservice.dto.AccountsDto;
-import com.microservice.dto.CustomerDto;
-import com.microservice.dto.ErrorResponseDto;
-import com.microservice.dto.ResponseDto;
+import com.microservice.dto.*;
 import com.microservice.entity.Accounts;
 import com.microservice.entity.Customer;
 import com.microservice.mapper.AccountsMapper;
@@ -15,6 +12,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +25,14 @@ import org.springframework.web.bind.annotation.*;
 public class AccountsController {
     @Autowired
     private AccountsService accountsService;
+
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
+
+@Value("${build.version}")
+    private String buildVersion;
 
     @Operation(
             summary = "create Account Details Rest API",
@@ -90,6 +98,24 @@ CustomerDto  customerDto = accountsService.findCustomerByMobileNo(mobile);
         accountsService.deleteAccount(customerId);
         return ResponseEntity.status(HttpStatus.OK).body("The Customer with Id"+customerId+"has been deleted ");
     }
+@GetMapping("/build-Version")
 
+    public ResponseEntity<String>getBuildVersion()
+{
+    return  ResponseEntity.ok(buildVersion);
+}
+
+    @GetMapping("/environment")
+
+    public ResponseEntity<String>getEnvironmentVersion()
+    {
+        return  ResponseEntity.ok(environment.getProperty("Maven_Home"));
+    }
+
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto>getContactInfo()
+    {
+        return ResponseEntity.ok(accountsContactInfoDto);
+    }
 
 }
